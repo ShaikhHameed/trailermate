043d362f21ad0a2b7c8b45fe14634c49566bde1b
= <=>
2024-01-25 00:03:33 +0530

    Movie page reset

--- app/watch-movie/page.js
+++ app/watch-movie/page.js
@@ -1,41 +1,49 @@
 'use client';
-import { useSearchParams } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { useEffect, useState, Suspense } from 'react';
 import DOMPurify from 'dompurify';
 
 export default function WatchMovie() {
-//   const [trailer, setTrailer] = useState(null);
-//   const searchParam = useSearchParams();
-//   const movieId = searchParam.get('id');
+  // const [trailer, setTrailer] = useState(null);
+  // const searchParam = useSearchParams();
+  // const movieId = searchParam.get('id');
 
-//   useEffect(() => {
-//     const getMovie = async () => {
-//       const fetchMovie = await fetch(`/api/getsingleMovie?id=${movieId}`, {
-//         method: 'GET',
-//       });
+  // const router = useRouter();
 
-//       const response = await fetchMovie.json();
-//       setTrailer(response.movies.trailer);
-//     };
+  // // const {id} = router.query();
 
-//     getMovie();
-//   }, [movieId]);
+  // // alert(id);
+
+  // useEffect(() => {
+  //   const getMovie = async () => {
+  //     const fetchMovie = await fetch(`/api/getsingleMovie?id=${movieId}`, {
+  //       method: 'GET',
+  //     });
+
+  //     const response = await fetchMovie.json();
+  //     setTrailer(response.movies.trailer);
+  //   };
+
+  //   getMovie();
+  // }, [movieId]);
 
   return (
-    <></>
-    // <Suspense fallback={<div>Loading...</div>}>
-    //   <div className="row">
-    //     <div className="col-md-8">
-    //       <iframe
-    //         className="rounded-4"
-    //         src={trailer}
-    //         title="YouTube video player"
-    //         frameBorder="0"
-    //         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-    //         allowFullScreen
-    //       />
-    //     </div>
-    //   </div>
-    // </Suspense>
+    <>
+      {/* <div className="row">
+        <div className="col-md-8">
+          <iframe
+            width="100%"
+            height="550px"
+            className="rounded-4"
+            src={trailer}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          />
+        </div>
+      </div> */}
+      <h1>Hello World</h1>
+      </>
   );
 }
