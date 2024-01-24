9eb873b22b0a875a8e18e8fad2eea81119b03217
= <=>
2024-01-24 22:52:47 +0530

    Remove Watch movie

--- app/watch-movie/page.js
+++ app/watch-movie/page.js
@@ -4,37 +4,38 @@ import { useEffect, useState, Suspense } from 'react';
 import DOMPurify from 'dompurify';
 
 export default function WatchMovie() {
-  const [trailer, setTrailer] = useState(null);
-  const searchParam = useSearchParams();
-  const movieId = searchParam.get('id');
+//   const [trailer, setTrailer] = useState(null);
+//   const searchParam = useSearchParams();
+//   const movieId = searchParam.get('id');
 
-  useEffect(() => {
-    const getMovie = async () => {
-      const fetchMovie = await fetch(`/api/getsingleMovie?id=${movieId}`, {
-        method: 'GET',
-      });
+//   useEffect(() => {
+//     const getMovie = async () => {
+//       const fetchMovie = await fetch(`/api/getsingleMovie?id=${movieId}`, {
+//         method: 'GET',
+//       });
 
-      const response = await fetchMovie.json();
-      setTrailer(response.movies.trailer);
-    };
+//       const response = await fetchMovie.json();
+//       setTrailer(response.movies.trailer);
+//     };
 
-    getMovie();
-  }, [movieId]);
+//     getMovie();
+//   }, [movieId]);
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div className="row">
-        <div className="col-md-8">
-          <iframe
-            className="rounded-4"
-            src={trailer}
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowFullScreen
-          />
-        </div>
-      </div>
-    </Suspense>
+    <></>
+    // <Suspense fallback={<div>Loading...</div>}>
+    //   <div className="row">
+    //     <div className="col-md-8">
+    //       <iframe
+    //         className="rounded-4"
+    //         src={trailer}
+    //         title="YouTube video player"
+    //         frameBorder="0"
+    //         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    //         allowFullScreen
+    //       />
+    //     </div>
+    //   </div>
+    // </Suspense>
   );
 }
