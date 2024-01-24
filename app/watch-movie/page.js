a61427bcfd2e96fc51ec3002af4f1b76f8206513
= <=>
2024-01-24 22:46:30 +0530

    Commit More

--- app/watch-movie/page.js
+++ app/watch-movie/page.js
@@ -1,45 +1,40 @@
 'use client';
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react"
-import { Suspense } from "react";
-import DOMPurify from "dompurify";
+import { useSearchParams } from 'next/navigation';
+import { useEffect, useState, Suspense } from 'react';
+import DOMPurify from 'dompurify';
 
+export default function WatchMovie() {
+  const [trailer, setTrailer] = useState(null);
+  const searchParam = useSearchParams();
+  const movieId = searchParam.get('id');
 
-export default function WatchMovie(){
+  useEffect(() => {
+    const getMovie = async () => {
+      const fetchMovie = await fetch(`/api/getsingleMovie?id=${movieId}`, {
+        method: 'GET',
+      });
 
-    const [Trailer,setTrailer] = useState(null);
+      const response = await fetchMovie.json();
+      setTrailer(response.movies.trailer);
+    };
 
-    const searchParam = useSearchParams();
-    const movieId = searchParam.get('id');
- 
-    useEffect(()=>{
-        const getMovie = async()=>{
+    getMovie();
+  }, [movieId]);
 
-            const fetchMovie = await fetch(`/api/getsingleMovie?id=${movieId}`,{
-                method:"GET",
-            });
-
-            const Response = await fetchMovie.json();
-            setTrailer(Response.movies.trailer);
-        }
-
-        getMovie();
-    },[]);
-
-
-    
-    return(
-
-        <>
-        <Suspense fallback={<div>Loading...</div>}>
-            <div className="row">
-                <div className="col-md-8">
-                    <iframe className="rounded-4" src={Trailer} title="YouTube video player"  frameBorder ="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen  />
-                </div>
-            </div>
-        </Suspense>
-        </>
-
-    )
-
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="row">
+        <div className="col-md-8">
+          <iframe
+            className="rounded-4"
+            src={trailer}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          />
+        </div>
+      </div>
+    </Suspense>
+  );
 }
